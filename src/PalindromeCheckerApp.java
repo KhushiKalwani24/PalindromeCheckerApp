16db0c16f8e9f18073757d99467f5894ceac2b01
KhushiKalwani24 <khushikalwani2412@gmail.com>
2026-02-20 10:31:00 +0530

    Base application setup

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,5 @@
 public class PalindromeCheckerApp {
     public static void main(String [] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 2.0");
-        System.out.println("System initialized successfully");
+
     }
 }
