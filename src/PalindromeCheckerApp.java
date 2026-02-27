39e3765473897506b03cd3bda7c7c1657d44612e
KhushiKalwani24 <khushikalwani2412@gmail.com>
2026-02-27 10:05:41 +0530

    UC12: Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,35 +1,83 @@
-import java.util.Scanner;
-
-public class PalindromeCheckerApp {
+import java.util.*;
 
 
-    public boolean checkPalindrome(String input) {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 
-        int start = 0;
-        int end = input.length() - 1;
 
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+class StackStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            stack.push(ch);
+        }
+
+        for (char ch : input.toCharArray()) {
+            if (ch != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
 
         return true;
     }
 }
 
-class UC11_ObjectOrientedPalindrome {
+
+class DequeStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char ch : input.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class PalindromeService {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String input) {
+        return strategy.check(input);
+    }
+}
+
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        PalindromeCheckerApp checker = new PalindromeCheckerApp();
 
-        boolean result = checker.checkPalindrome(input);
+        // Choose strategy dynamically
+        PalindromeStrategy strategy = new StackStrategy();
+        // You can switch to:
+        // PalindromeStrategy strategy = new DequeStrategy();
+
+        PalindromeService service = new PalindromeService(strategy);
+
+        boolean result = service.execute(input);
 
         System.out.println("input: " + input);
         System.out.println("is palindrome:" + result);
